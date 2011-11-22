4a4643197ff4b78b2b758d1190ba77f5c239f6ba
MPink <m_pink@live.co.uk>
2011-11-22 14:45:52 +0100

    mission 1

--- scripts/nextjump.lua
+++ scripts/nextjump.lua
@@ -15,3 +15,43 @@ function getNextSystem(nowsys, finalsys)
         return mynextsys
     end
 end
+
+-- return a table of systems for a route from start to end
+function FindRoute ( start_sys_, end_sys_ )
+   local checked_sys_ary = {start_sys_}
+   local routes = {{start_sys_}} -- a table of tables
+   
+   while #routes>0 do
+      local current_rt = table.remove(routes,1)
+      local current_sys = current_rt[#current_rt]
+      local rt_string = ""
+      for k,v in pairs(current_rt) do rt_string=string.format("%s %s",rt_string,v:name()) end
+
+      -- loop through adjacent systems
+      for _,sys in pairs(current_sys:adjacentSystems()) do
+         -- check system has not already been added to a route
+         if not Contains(checked_sys_ary,sys) then
+            local next_route = Copy(current_rt)
+            table.insert(next_route,sys)
+            table.insert(checked_sys_ary,sys)
+            table.insert(routes,next_route)
+            -- return found route
+            if sys==end_sys_ then return next_route end
+         end
+      end
+   end
+   
+   return {}
+end
+
+function Contains (table_,value_)
+   for _,v in pairs(table_) do
+      if v==value_ then return true end
+   end
+   return false
+end
+function Copy (table_)
+   local result = {}
+   for _,v in pairs(table_) do table.insert(result,v) end
+   return result
+end
